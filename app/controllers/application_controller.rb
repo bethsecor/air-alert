39ce895b8ba573c356fc76530d5260c00c5e3c3d
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-29 15:50:39 -0700

    Styling for landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if params["controller"] == "welcome"
+      "welcome"
+    else
+      "application"
+    end
+  end
 end
