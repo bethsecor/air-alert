fddb0a43abd68b8e5ba8d7ba40ce8c7a49cca572
Beth Secor <elizabeth.a.secor@gmail.com>
2016-03-09 15:48:32 -0700

    Refactor google map view and tweet in view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,6 @@
 class TweetsController < ApplicationController
   def create
     air_quality_tweet = current_user.tweet(twitter_params[:message])
-    # binding.pry
     flash[:tweet] = "Successfully tweeted!"
     redirect_to airquality_path
   end
--- spec/features/authenticated_user_can_tweet_air_qualities_spec.rb
+++ spec/features/authenticated_user_can_tweet_air_qualities_spec.rb
@@ -19,4 +19,20 @@ RSpec.feature "AuthenticatedUserCanTweetAirQualities", type: :feature do
       end
     end
   end
+
+  it "can tweet the current air quality" do
+    visit root_path
+
+    VCR.use_cassette("breezometer_service#air_quality") do
+      click_on "Login with Twitter"
+      expect(current_path).to eq(airquality_path)
+
+      VCR.use_cassette("twitter#tweet user location air quality message") do
+        click_on "Tweet"
+
+        expect(page).to have_content("Successfully tweeted!")
+        expect(current_path).to eq(airquality_path)
+      end
+    end
+  end
 end
